42d838e017c4ae18ddc8dbd293141fa2ffb7b42c
Chris Moylan <chris@chrismoylan.com>
2019-11-12 07:10:12 -0600

    migrate/reset works

--- src/schema.lisp
+++ src/schema.lisp
@@ -10,12 +10,33 @@
                 :execute
                 :retrieve-all
                 :retrieve-one)
-  (:export :migrate))
+  (:export :migrate
+           :reset
+           :seed))
 
 (in-package :uuidnet.db.schema)
 
+
 (defun migrate ()
-  "Run all migrations to set up the database"
+  "Run all migrations to set up the database."
+  (create-users)
+  (create-messages)
+  )
+
+(defun reset ()
+  "Drop all tables and recreate."
+  (drop-all-tables)
+  (migrate))
+
+(defun seed ()
+  "Create basic test data."
+
+  )
+
+
+;; --- Individual migrations --- ;;
+(defun create-users ()
+  "Create the users table."
   (with-connection (db)
     (execute
      (create-table (:user :if-not-exists nil)
@@ -25,13 +46,29 @@
           (password :type 'text :not-null t)
           (uuid :type 'text :not-null t :unique t)
           (created_at :type 'timestamp :not-null t)
-          (updated_at :type 'timestamp :not-null t))))
+          (updated_at :type 'timestamp :not-null t))))))
 
+
+(defun create-messages ()
+  "Create the messges table."
+  (with-connection (db)
     (execute
      (create-table (:message :if-not-exists nil)
          ((id :type 'serial :primary-key t)
-          (sender_id :type 'int :not-null t :references '(:user :id))
-          (recipient_id :type 'int :not-null t :references '(:user :id))
+          (sender_id :type 'int :not-null t)
+          (recipient_id :type 'int :not-null t)
           (body :type 'text :not-null t)
           (created_at :type 'timestamp :not-null t)
           (updated_at :type 'timestamp :not-null t))))))
+
+(defun drop-all-tables ()
+  "Drop all tables from the database."
+
+  (with-connection (db)
+    (execute
+     (drop-table :user :if-exists t)
+     )
+    (execute
+     (drop-table :message :if-exists t)
+     ))
+  )
