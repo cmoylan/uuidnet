b9dd74fb00402b3d8bac25eb2665944c0eed3b75
Chris Moylan <chris@chrismoylan.com>
2019-12-09 20:05:50 -0600

    message model

--- src/models/message.lisp
+++ src/models/message.lisp
@@ -11,7 +11,8 @@
                 :execute
                 :retrieve-all
                 :retrieve-one)
-  (:export ))
+  (:export :add-message
+           ))
 
 (in-package :uuidnet.models.message)
 
@@ -23,3 +24,43 @@
   body
   created_at
   updated_at)
+
+
+(defun add-message (&key body sender_id recipient_id reply_id)
+  (with-connection (db)
+    (execute
+     (insert-into :messages
+       (set= :sender_id sender_id
+             :recipient_id recipient_id
+             :reply_id reply_id
+             :body body
+             :created_at (local-time:now)
+             :updated_at (local-time:now))))))
+
+
+(defun find-messages-by-sender (sender_id)
+  (with-connection (db)
+    (retrieve-all
+     (select :*
+        (from :messages)
+       (where (:= :sender_id sender_id)))
+     :as 'message)))
+
+
+(defun find-messages-in-thread (message_id)
+  (with-connection (db)
+    (retrieve-all
+     (select :*
+        (from :messages)
+       (where (:= :reply_id message_id)))
+     :as 'message)))
+
+
+(defun find-messages-by-sender-recipient (sender_id recipient_id)
+  (with-connection (db)
+    (retrieve-all
+     (select :*
+        (from :messages)
+       (where (:and (:= :sender_id sender_id)
+                    (:= :recipient_id recipient_id))))
+     :as 'message)))
